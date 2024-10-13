d768991f49eab752d7afed6054d69f2cb1f94a17
Claire Masiga <ClaireMasiga22@gmail.com>
2024-10-13 15:06:01 +0300

    work

--- site.php
+++ site.php
@@ -10,6 +10,23 @@
 <?php
 echo("Hello World");
 echo"<h1>Claire's Site</h1>";
+echo"<hr>";
+echo"<p>This is my site</p>";
+$characterName="Ariana";
+$characterAge=23;
+echo "There was once a girl $characterName<br>";
+echo "She was $characterAge<br>";
+echo "However she hated being $characterAge<br>";
+$characterName="Tom";
+echo "One day she met  $characterName<br>";
+echo "He made her feel so comfortable about her age<br>";
+$phrase="To be continued";
+$age=40;
+$gpa=3.5;
+$isMale=false;
+$gpa=3.5;
+echo "$phrase";
+
 ?>
 </body>
 </html>
